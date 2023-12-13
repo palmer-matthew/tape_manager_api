30aa17a296a0689afd57b787faf4ca3b6bf03cc5
Matthew Palmer <palmer.matthew4567@gmail.com>
2023-12-13 10:57:44 -0500

    feat: add querystring parser

--- api/routes/tapes.py
+++ api/routes/tapes.py
@@ -1,5 +1,20 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+
+paramParser = reqparse.RequestParser()
+paramParser.add_argument('order', type=str, location='args')
+paramParser.add_argument('page', type=int, location='args')
+paramParser.add_argument('per_page', type=int, location='args')
+paramParser.add_argument('search', type=str, location='args')
 
 class Tapes(Resource):
     def get(self):
-        return { 'routes': '/api/tapes' }
+        params = paramParser.parse_args()
+
+        if not params['per_page']:
+            params['per_page'] = 10
+        
+        if not params['page']:
+            params['page'] = 1
+
+
+        return { 'routes': '/api/tapes', 'args': params }
