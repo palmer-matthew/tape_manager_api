1453d24e5270b089145bf081d393569bb650c34c
Matthew Palmer <palmer.matthew4567@gmail.com>
2023-12-15 21:31:54 -0500

    chore!: clean up model class

---
+++ api/models/tape.py
@@ -0,0 +1,7 @@
+from api import db
+from datetime import datetime
+from pytz import timezone
+
+# class TapeMedia(db.Model):
+#     __tablename__ = 'media'
+#     mediaID = db.Column(db.Integer, prima)
