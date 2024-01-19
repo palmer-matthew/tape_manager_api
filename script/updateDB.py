bb0839040cfa5c2c3836e897b9876d68d5036fe3
Matthew Palmer <palmer.matthew4567@gmail.com>
2024-01-18 20:13:45 -0500

    feat: start db update script

--- script/updateDB.py
+++ script/updateDB.py
@@ -1,3 +1,14 @@
 # Python Script To Update Database With Previous Data From Excel
-# from csv import DictReader
+from csv import DictReader
 # from mysql import connector
+
+def read_csv(path: str):
+    with open(path, newline='', encoding='utf-8-sig') as file:
+        reader = DictReader(file)
+        rows = []
+        for row in reader:
+            rows.append(row)
+    return rows
+
+if __name__ == '__main__':
+    pass
