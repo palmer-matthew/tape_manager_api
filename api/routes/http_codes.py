2f543ccf5cb186372ec479d55d8a8c3a4a8056d4
Matthew Palmer <palmer.matthew4567@gmail.com>
2024-01-24 21:27:55 -0500

    chore: cleanup response codes

---
+++ api/routes/http_codes.py
@@ -0,0 +1,25 @@
+INTERNAL_ERROR_STATUS_CODE = {
+    'result' : [],
+    'additional': {},
+    'message': 'Something went wrong.',
+    'code': 500
+} 
+
+BAD_STATUS_CODE = {
+    'result' : [],
+    'additional': {},
+    'message': 'Request is Invalid',
+    'code': 400
+} 
+
+EMPTY_STATUS_CODE = {
+    'result' : [],
+    'additional': {
+        'length': 0,
+        'page': 1,
+        'total_pages': 1,
+        'per_page': 0
+    },
+    'message': 'No records were found',
+    'code': 204
+}
