736bb7d24955e8f73201a9c6c5a92e718f39da41
Matthew Palmer <palmer.matthew4567@gmail.com>
2024-03-04 21:36:45 -0500

    fix: add base route

--- api/routes/home.py
+++ api/routes/home.py
@@ -2,4 +2,13 @@ from flask_restful import Resource
 
 class Home(Resource):
     def get(self):
-        return { 'route': '/api'}
+        return { 
+            'routes': {
+                'base': '/api',
+                'get-tapes': '/api/tapes',
+                'get-tape': '/api/tape/<id:string>',
+                'patch-tape': '/api/tape/<id:string>',
+                'put-tape': '/api/tape/<id:string>',
+                'delete-tape': '/api/tape/<id:string>'
+            }
+        }
